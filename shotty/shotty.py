83c21a081e01eb8fac4ec1f120fe3e28d0b66f37
Juha Fagerstedt <sashadb16@gmail.com>
2018-03-21 08:10:16 +0200

    Handle error from AWS when stopping a pending instance or start a stopping one

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,4 +1,5 @@
 import boto3
+import botocore
 import click
 
 session = boto3.Session(profile_name='shotty')
@@ -16,9 +17,61 @@ def filter_instances(project):
         return instances
 
 @click.group()
+def cli():
+    """Shotty manages instances"""
+
+@cli.group('volumes')
+def volumes():
+    """Commands for volumes"""
+@volumes.command('list')
+@click.option('--project', default=None,
+    help="Only volumes for project (tag Project:<Name>)")
+def list_volumes(project):
+    "List EC2 volumes"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        for v in i.volumes.all():
+            print(", ".join((
+                v.id,
+                i.id,
+                v.state,
+                str(v.size) + "GiB",
+                v.encrypted and "Encrypted" or " Not Encrypted"
+            )))
+    return
+
+@cli.group('instances')
 def instances():
         """Commands for instances"""
 
+@instances.command('snapshot',
+    help="Create snapshots of all volumes")
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<Name>)")
+def create_snapshot(project):
+    "Create snapshot for EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0} ...".format(i.id))
+
+        i.stop()
+        i.wait_until_stopped()
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}".format(v.id))
+            v.create_snapshot(Description="Created by snaphot-analyzer 3000")
+
+        print("Starting {0}".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Job done!")
+
+    return
 @instances.command('list')
 @click.option('--project', default=None,
     help="Only instances for project (tag Project:<Name>)")
@@ -50,7 +103,11 @@ def stop_instances(project):
 
     for i in instances:
         print("Stopping {0}...".format(i.id))
-        i.stop()
+        try:
+            i.stop()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not stop {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
@@ -64,9 +121,13 @@ def start_instances(project):
 
     for i in instances:
         print("Starting {0}...".format(i.id))
-        i.start()
+        try:
+            i.start()
+        except botocore.exceptions.ClientError as e:
+            print(" Could not start {0}. ".format(i.id) + str(e))
+            continue
 
     return
 
 if __name__ == '__main__':
-    instances()
+    cli()
